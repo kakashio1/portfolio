0d31569a9c93c3a755a855aec33109d0c5392ee0
kakashi <167419211+kakashio1@users.noreply.github.com>
2024-10-09 20:27:23 +0530

    Add files via upload
    this is my portfolio

---
+++ newp/contact/js.js
@@ -0,0 +1,10 @@
+let op1 = document.getElementById(op1);
+let op2 = document.getElementById(op2);
+let op3 = document.getElementById(op3);
+let op4 = document.getElementById(op4);
+
+
+let scrore = 0;
+
+op1.addEventListener('click',console.log(score++1))
+
